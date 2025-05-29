6c45ee54a21df617f025472a1c2a19ac0099cd09
Alejandra Castro Rojas <44410820+alejacr1997@users.noreply.github.com>
2025-05-29 18:00:23 -0500

    Add files via upload

---
+++ src/test/java/com/project/springboot/config/SpringConfigApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springboot.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
