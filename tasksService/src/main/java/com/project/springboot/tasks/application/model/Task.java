0a8530b612bd0143834bcbd83e8a1c4c14e3c62e
alejacr1997 <alejandracr1997@gmail.com>
2025-05-29 20:35:20 -0500

    Ajustes para ejecución del codigo

--- tasksService/src/main/java/com/project/springboot/tasks/application/model/Task.java
+++ tasksService/src/main/java/com/project/springboot/tasks/application/model/Task.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Document(collection = "tasks")
@@ -12,10 +14,11 @@ import lombok.Data;
 public class Task {
 	
 	@Id
-	private Long idUsuario;
+	private String idUsuario;
 	private String title;
 	private String description;
 	private boolean status;
 	private Date dueDate;
+	private Date creationDate;
 
 }
