9840e4a48beb0be723723a1ec189eec261407d36
alejacr1997 <alejandracr1997@gmail.com>
2025-06-21 19:34:20 -0500

    Cambios servicios update Task

--- tasksService/src/main/java/com/project/springboot/tasks/commons/utils/Utils.java
+++ tasksService/src/main/java/com/project/springboot/tasks/commons/utils/Utils.java
@@ -8,12 +8,8 @@ public class Utils {
 	public Task updateFields(UpdateTaskRequest request, Task task){
 		if(request.getDueDate() != null) 
 			task.setDueDate(request.getDueDate());
-		if(request.getDescription() != null && !request.getDescription().isEmpty())
-			task.setDescription(request.getDescription());
 		if(request.getTitle() != null && !request.getTitle().isEmpty())
 			task.setTitle(request.getTitle());
-		if(!task.isStatus()) 
-			task.setStatus(request.isStatus());
 		return task;
 	}
 }
